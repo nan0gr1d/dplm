1d1000213860904e3fdcbc9944d99c322309196c
Urban <prestofan@gmail.com>
2024-12-21 17:51:02 +0300

    Улучшил интерфейс,

--- djaprj/shop/models.py
+++ djaprj/shop/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=30)
-    price = models.DecimalField(decimal_places=2, max_digits=10)
     description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=7)
--- djaprj/shop/views.py
+++ djaprj/shop/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from .models import Product
 from .forms import ProductAdd
 
-
+# Отображаем домашнюю страницу
 def homepage(request):
     return render(request, "home.html")
 
+# Отображаем каталог товаров
 def catalog(request):
     products = Product.objects.all()
     context = {
@@ -13,9 +14,10 @@ def catalog(request):
     }
     return render(request, "catalog.html", context=context)
 
+# Обрабатываем данные из формы добавления товара
 def process_form_data(title, price):
     error = ''
-    if int(price) <= 0:
+    if price <= 0:
         error = 'Product price must be positive number!'
         return error
 
@@ -26,32 +28,35 @@ def process_form_data(title, price):
             break
     return error
 
+
+# Добавляем новый товар в каталог
 def add_product(request):
-    error = ''
+    message = ''
     if request.method == 'POST':
         form = ProductAdd(request.POST)
         if form.is_valid():
             title = form.cleaned_data['title']
             description = form.cleaned_data['description']
-            price = int(form.cleaned_data['price'])
+            price = float(form.cleaned_data['price'])
+            # Проверка корректности введенных данных
             error = process_form_data(title, price)
-            if not error:
+            if error:
+                message = error
+            else:
                 Product.objects.create(title=title, description=description, price=price)
                 message = f"Product {title} added."
-                context = {
-                    'message': message,
-                }
-                return render(request, 'home.html', context=context)
     else:
         form = ProductAdd()
     context = {
         'form': form,
-        'error': error,
+        'message': message,
     }
     return render(request, 'product_add.html', context=context)
 
+# Удаляем товар из каталога по идентификатору
 def delete_product(request):
     id = request.POST.get("id")
+    # Есть ли товар с указанным идентификатором в каталоге?
     product = Product.objects.get(id=id)
     if product:
         title = product.title
@@ -59,7 +64,9 @@ def delete_product(request):
         message = f"Product {title} (id={id}) deleted."
     else:
         message = f"Product (id={id}) not found."
+    products = Product.objects.all()
     context = {
                 'message': message,
+                'products': products
                 }
-    return render(request, 'home.html', context=context)
+    return render(request, 'catalog.html', context=context)
